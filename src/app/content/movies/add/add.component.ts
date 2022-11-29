d5f537087f0258340980adb66b11b3a201eca1c4
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-11-28 21:56:15 -0500

    Finished code

--- src/app/content/movies/add/add.component.ts
+++ src/app/content/movies/add/add.component.ts
@@ -10,11 +10,11 @@ import { MoviesService } from 'src/app/services/movies.service';
 export class MovieAddComponent implements OnInit {
 
   form: any = {
-    name: String, 
-    year: String, 
-    director: String,
-    genre: String,
-    runtime: Number
+    name: null, 
+    year: null, 
+    director: null,
+    genre: null,
+    runtime: null
   }
 
   isSuccessfull = true;
@@ -41,7 +41,7 @@ export class MovieAddComponent implements OnInit {
   }
   
   backToList(): void {
-    this.router.navigate(['/books/list']);
+    this.router.navigate(['/movies/list']);
   }
 
 }
--- src/app/content/movies/edit/edit.component.ts
+++ src/app/content/movies/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieEditComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    _id: null,
+    name: null, 
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(private movieService: MoviesService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe({
+        next: params => {
+          console.log(params);
+          this.form._id = params['id'];
+
+          this.movieService.getMovie(this.form._id)
+            .subscribe({
+              next: data => {
+                console.log(data);
+                this.form = data.movie
+              },
+              error: err => {
+                this.errorMessage = err.error.message;
+                this.isSuccessfull = false;
+              }
+            })
+        }, 
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
+
+  onSubmit():void {
+    this.movieService.editMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.backToList();          
+        }, 
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      });
+  }
+  
+  backToList(): void {
+    this.router.navigate(['/movies/list']);
   }
 
 }
